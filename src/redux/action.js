9c14e624ab289e6608c4869eda2284a8faaf47a0
jose-Abel <joseabel.ramirezfrontany@gmail.com>
2022-05-31 23:52:37 -0400

    Configuring cors in the redux action

--- src/redux/action.js
+++ src/redux/action.js
@@ -1,11 +1,9 @@
 export const CHANGE_GREETING_SUCCESS = 'rails-react-hello-world/CHANGE_GREETING_SUCCESS';
 export const CHANGE_GREETING_FAILURE = 'rails-react-hello-world/CHANGE_GREETING_FAILURE';
 
-const baseURL = '/random_greetings';
-
 export const changeGreetings = () => async (dispatch) => {
   try {
-    const response = await fetch(`${baseURL}`);
+    const response = await fetch('http://localhost:3000/random_greetings');
     const data = await response.json();
 
     dispatch({ type: CHANGE_GREETING_SUCCESS, payload: data });
